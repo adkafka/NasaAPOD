495464b9703c3b4c3b74200077d3c049abf9054c
Adam Kafka <adkafka@gmail.com>
2017-05-23 15:54:56 -0400

    Continuied with CLI changes and refactoring.
    Next step will be to actually save the media...

--- build.sbt
+++ build.sbt
@@ -7,4 +7,4 @@ scalaVersion := "2.12.2"
 // Json parser
 libraryDependencies += "org.json4s" %% "json4s-native" % "3.5.2"
 // Time manipulation
-libraryDependencies += "com.github.nscala-time" %% "nscala-time" % "2.16.0"
+//libraryDependencies += "com.github.nscala-time" %% "nscala-time" % "2.16.0"
--- src/main/scala/Grab.scala
+++ src/main/scala/Grab.scala
@@ -5,6 +5,7 @@ import scala.io.Source //Read files, local and remote
 import java.time.LocalDate //DateTime ops
 import java.time.format.DateTimeFormatter //Format datetime
 
+
 object Grab { 
     // TODO, multiple parameter options
     /* 
@@ -12,36 +13,31 @@ object Grab {
          * Default to both == today
              * Or, start date is last file in directory... may introduce errors
          * yyyy-mm-dd
-         * Start on begin date. Create Date obj, convert to string with format.
-         * Use + 1.days (http://alvinalexander.com/scala/scala-number-nnumeric-date-formatting-casting-examples)
      * Add configuration to one file
          * API key
          * Destination Directory
          * Directory to put sym links in
+     * Retry on HTTP failures?
+         * Abstract HTTP stuff to one object. 
+         * Have to string and to file methods
      */
 
     val fmt = DateTimeFormatter.ofPattern("yyyy-MM-dd")
+    // Put these in a settings file
+    val pod_dir = "/Users/adam/Desktop/NasaDailyPics/"
+    val screensaver_dir = "/Users/adam/Pictures/Screen Saver Pics/"
 
     def main(args: Array[String]) = {
         // Grab start and end dates from agrs
         val (startDate, endDate) = parseArgs(args)
-        printf("Grabbing NASA POD from %s -> %s\n",startDate.format(fmt),
+        printf("[*] Grabbing NASA POD from %s -> %s\n",startDate.format(fmt),
             endDate.format(fmt))
 
         // For every date from start to end inclusive...
         val numberOfDays = startDate.until(endDate).getDays()
         for (diff <- 0 to numberOfDays)
             // Grab the file if we don't already have it
-            CheckAndGrab(startDate.plusDays(diff))
-
-
-
-        //MediaGrabber.Grab()
-        //MediaGrabber.Grab("2017-05-15")
-    }
-
-    def CheckAndGrab(date: LocalDate) = {
-        printf("Grabbing for day: %s\n",date.format(fmt))
+            MediaGrabber.CheckAndGrab(startDate.plusDays(diff))
     }
 
     def parseArgs(args: Array[String]) = {
@@ -57,18 +53,38 @@ object Grab {
     }
 
     def error(error_msg: String) = {
-        System.err.println(error_msg)
+        System.err.println("[!] "+error_msg)
         System.exit(1)
     }
 }
 
 
 object MediaGrabber{
+    def CheckAndGrab(date: LocalDate) = {
+        val date_str = date.format(Grab.fmt)
+        val dest_dir = new java.io.File(Grab.pod_dir)
+        // Check if file is present in pod_dir
+        val matches = dest_dir.listFiles.filter(_.isFile).toList.filter{ file =>
+            file.getName.startsWith(date_str)
+        }
+        // If it is, grab image for this date
+        if(matches.length==0){
+            GrabMedia(date_str)
+        }
+        else{
+            // Otherwise, continue
+            printf("[-] File already exists for date: %s, continuing on next date\n",
+                date_str)
+        }
+
+    }
+
     def GetAPIKey: String = {
         val key = Source.fromFile("API_KEY")
         try key.mkString.stripLineEnd finally key.close()
     }
-    def FetchJson(date : String = "") : JsonAST.JValue = {
+
+    def FetchJson(date : String = "") : Option[JsonAST.JValue] = {
         val API_KEY = MediaGrabber.GetAPIKey
         var URL = "https://api.nasa.gov/planetary/apod"
         URL = URL.concat("?api_key=%s".format(API_KEY))
@@ -76,37 +92,46 @@ object MediaGrabber{
             URL = URL.concat("&date=%s".format(date))
         }
         URL = URL.concat("&hd=True")
-        var jsonAst : JsonAST.JValue = null
         try{
             val json = Source.fromURL(URL)
-            jsonAst = parse(json.mkString)
-            println(pretty(render(jsonAst)))
+            Some(parse(json.mkString))
         }
         catch{
-            case io : java.io.IOException  => println("Server returned bad HTTP response code\n"+io)
-            case e  : Throwable => e.printStackTrace
+            case io : java.io.IOException  => 
+                printf("[-] Server returned bad HTTP response code:\n\t%s\n",io)
+                None
+            case e  : Throwable => 
+                e.printStackTrace
+                None
         }
 
-        return jsonAst
     }
-    def Grab(date : String = "") = {
-        val jsonTree = FetchJson(date)
 
-        implicit val formats = DefaultFormats
-        val resp = jsonTree.extract[Response]
+    def GrabMedia(date : String = "") = {
+        printf("[+] Grabbing for day: %s\n",date)
+        FetchJson(date) match {
+            case Some(jsonTree) => {
+                implicit val formats = DefaultFormats
+                val resp = jsonTree.extract[Response]
 
-        printf("Date: %s\nTitle: %s\nType: %s\n",resp.date,resp.title,resp.media_type)
+                printf("[+]\tDate: %s\n\tTitle: %s\n\tType: %s\n",resp.date,
+                    resp.title,resp.media_type)
 
-        resp.media_type match {
-            case "image" => GrabImage(resp)
-            case "video" => GrabVideo(resp)
-            case default => printf("Unknown media type found: %s\n",default)
+                resp.media_type match {
+                    case "image" => GrabImage(resp)
+                    case "video" => GrabVideo(resp)
+                    case default => printf("[!] Unknown media type found: %s\n",default)
+                }
+            }
+            case None  => printf("[-] Could not query NASA API\n")
         }
     }
-    def GrabImage(resp: Response){
+    def GrabImage(resp: Response) = {
 
     }
-    def GrabVideo(resp: Response){
+
+    def GrabVideo(resp: Response) = {
+
     }
 }
 
