69bc47c89a1028f70a72e7f1c57e4de682f4317e
Adam Kafka <adkafka@gmail.com>
2017-06-01 00:28:25 -0400

    Added external config file and tweaked performance
    Configuration is loaded from a single file: config.json.
    
    Additionally, the script was tested on all dates since 2013-02-08 (original script creation date). Errors were fixed along the way.
    Mostly youtube-dl: Added max file size, changed format specifiactions to just take highest quality mp4, then highest quality image overall, while limiting the isize of the file to prevent huge downloads.

--- src/main/scala/Grab.scala
+++ src/main/scala/Grab.scala
@@ -1,6 +1,6 @@
-//import spray.json._ //Json parser
-import org.json4s._
-import org.json4s.native.JsonMethods._
+import org.json4s._ //Json parser
+import org.json4s.native.JsonMethods._ //Json parser
+
 import scala.io.Source //Read files, local and remote
 import java.time.LocalDate //DateTime ops
 import java.time.format.DateTimeFormatter //Format datetime
@@ -8,30 +8,19 @@ import java.time.format.DateTimeFormatter //Format datetime
 import sys.process._ //Piping
 import java.net.URL //Donwload
 import java.io.File // Save
-import scala.language.postfixOps //Allow !! at end of pipe call
-
 import java.nio.file._ // Create hard link
+import scala.language.postfixOps //Allow '!!' and '!' at end of pipe call
 
 object Grab { 
-    // TODO
     /* 
-     * Grab [start yyyy-mm-dd] [end yyyy-mm-dd]
-         * Default to both == today
-             * Or, start date is last file in directory... may introduce errors
-             * Or, if one date given, only do that date...
-     * Add configuration to one file
-         * API key
-         * Destination Directory
-         * Directory to put sym links in
+     * Default params...
+         * Or, start date is last file in directory... may introduce errors
+         * Or, if one date given, only do that date...
      * Read through and refactor some things
          * Use Option when available
          * What to return from funcs...
      */
-
     val fmt = DateTimeFormatter.ofPattern("yyyy-MM-dd")
-    // Put these in a settings file
-    val pod_dir = "/Users/adam/Desktop/NasaDailyPics2/"
-    val screensaver_dir = "/Users/adam/Desktop/ScreenSaverPics/"
 
     def main(args: Array[String]) = {
         // Grab start and end dates from agrs
@@ -40,11 +29,13 @@ object Grab {
             endDate.format(fmt))
 
         // For every date from start to end inclusive...
-        val numberOfDays = java.time.temporal.ChronoUnit.DAYS.between(startDate,endDate).toInt
+        val numberOfDays = java.time.temporal.
+            ChronoUnit.DAYS.between(startDate,endDate).toInt
         for (diff <- 0 to numberOfDays){
             // Grab the file if we don't already have it
             MediaGrabber.CheckAndGrab(startDate.plusDays(diff))
         }
+        println("[+] No more dates, done")
     }
 
     def parseArgs(args: Array[String]) = {
@@ -65,38 +56,76 @@ object Grab {
     }
 }
 
+object Config {
+    var config : Config_Opts = null
+    val config_path = "./config.json"
+    
+    case class Config_Opts (
+        pod_dir : String = "",
+        screensaver_dir : String = "",
+        api_key : String = "")
+
+    def pod_dir() = {
+        read_config
+        // If it is still empty...
+        if (config.pod_dir=="") Grab.error("No POD directory set in config file")
+
+        config.pod_dir
+    }
+    def screensaver_dir() = {
+        read_config
+
+        config.screensaver_dir
+    }
+    def api_key() = {
+        read_config
+        // If it is still empty...
+        if (config.api_key=="") Grab.error("No API key found in config file")
+
+        config.api_key
+    }
+
+    def read_config() = {
+        if (config == null){
+            // Read file
+            val jsonTree = parse(Source.fromFile(config_path).mkString)
+            //Get Case class
+            implicit val formats = DefaultFormats
+            config = jsonTree.extract[Config_Opts]
+        }
+    }
+}
+
 
 object MediaGrabber{
     def CheckAndGrab(date: LocalDate) = {
         val date_str = date.format(Grab.fmt)
-        val dest_dir = new java.io.File(Grab.pod_dir)
+        val dest_dir = new java.io.File(Config.pod_dir)
+
+        // Make sure directory in config is present
+        if(!dest_dir.exists){
+            Grab.error("Invalid pod_dir: %s".format(Config.pod_dir))
+        }
+
         // Check if file is present in pod_dir
         val matches = dest_dir.listFiles.filter(_.isFile).toList.filter{ file =>
             file.getName.startsWith(date_str)
         }
-        /*
+
         // If it is, grab image for this date
         if(matches.length==0){
-            */
             GrabMedia(date_str)
-            /*
         }
         else{
             // Otherwise, continue
             printf("[-] File already exists for date: %s, continuing on next date\n",
                 date_str)
         }
-        */
 
     }
 
-    def GetAPIKey: String = {
-        val key = Source.fromFile("API_KEY")
-        try key.mkString.stripLineEnd finally key.close()
-    }
-
-    def FetchJson(date : String = "") : Option[JsonAST.JValue] = {
-        val API_KEY = MediaGrabber.GetAPIKey
+    def FetchJson(date : String = "") : Option[Response] = {
+        val API_KEY = Config.api_key
         var URL = "https://api.nasa.gov/planetary/apod"
         URL = URL.concat("?api_key=%s".format(API_KEY))
         if (date != ""){
@@ -105,7 +134,10 @@ object MediaGrabber{
         URL = URL.concat("&hd=True")
         try{
             val json = Source.fromURL(URL)
-            Some(parse(json.mkString))
+            val jsonTree = parse(json.mkString)
+
+            implicit val formats = DefaultFormats
+            Some(jsonTree.extract[Response])
         }
         catch{
             case io : java.io.IOException  => 
@@ -121,10 +153,7 @@ object MediaGrabber{
     def GrabMedia(date : String = "") = {
         printf("[+] Grabbing for day: %s\n",date)
         FetchJson(date) match {
-            case Some(jsonTree) => {
-                implicit val formats = DefaultFormats
-                val resp = jsonTree.extract[Response]
-
+            case Some(resp) => {
                 printf("[+]\tDate: %s\n\tTitle: %s\n\tType: %s\n",resp.date,
                     resp.title,resp.media_type)
 
@@ -133,9 +162,11 @@ object MediaGrabber{
                     case "video" => GrabVideo(resp)
                     case default => printf("[!] Unknown media type found: %s\n",default)
                 }
-                printf("[+] Done\n")
+                printf("[+] Done with %s\n",resp.date)
             }
-            case None  => printf("[-] Could not query NASA API\n")
+            case None  => {
+                printf("[-] Could not query NASA API. Make sure you are connected to the internet and you have a valid api_key in the config.json file\n")
+            }
         }
     }
     
@@ -147,7 +178,7 @@ object MediaGrabber{
         val url = if (resp.hdurl!="") resp.hdurl else resp.url
         val file_ext = url.split('.').last
         val out_fn = MakeFilename(resp)+"."+file_ext
-        val out_full = Grab.pod_dir+out_fn
+        val out_full = Config.pod_dir+out_fn
         printf("[+] Grabbing image from URL: %s\n",resp.hdurl)
         printf("[+] Saving as filename: %s\n", out_full)
 
@@ -160,31 +191,37 @@ object MediaGrabber{
         }
 
         // Link to screen saver path
-        try{
-            val destination = Grab.screensaver_dir+out_fn
-            Files.createLink(Paths.get(destination), Paths.get(out_full));
-        }catch{
-            case exists : java.nio.file.FileAlreadyExistsException => {
-                printf("[-] Hard link already exists\n")
-            }
-            case e : Throwable => {
-                printf("[!] Error creating link!")
-                e.printStackTrace
+        if (Config.screensaver_dir != ""){
+            try{
+                val destination = Config.screensaver_dir+out_fn
+                Files.createLink(Paths.get(destination), Paths.get(out_full));
+            }catch{
+                case exists : java.nio.file.FileAlreadyExistsException => {
+                    printf("[-] Hard link already exists\n")
+                }
+                case e : Throwable => {
+                    printf("[!] Error creating link!")
+                    e.printStackTrace
+                }
             }
         }
     }
 
     def GrabVideo(resp: Response) = {
         val out_fn = MakeFilename(resp)
-        val out_full = Grab.pod_dir+out_fn
+        val out_full = Config.pod_dir+out_fn
 
-        val exit_status = "youtube-dl -f 38/37/84/22/85/82/83/18/17/35/34/5 "+
-                            "-o "+out_full+".%(ext)s"+
-                            " --restrict-filenames "+resp.url !
+        printf("[+] Grabbing video from URL: %s\n",resp.url)
+        printf("[+] Saving as filename: %s\n", out_full)
+
+        val exit_status = "youtube-dl --no-progress "+
+            "-f bestvideo[ext=mp4][filesize<100M]+bestaudio[ext=m4a]/best[ext=mp4][filesize<100M]/best[filesize<100M] "+
+            "-o "+out_full+".%(ext)s"+
+            " --restrict-filenames "+resp.url !
 
         if (exit_status != 0){
-            printf("Youtube-dl failed with exit status: %d\n",exit_status)
-            printf("Skipping this day because there is no easy way to download media\n")
+            printf("[-] Youtube-dl failed with exit status: %d\n",exit_status)
+            printf("[-] Skipping this day because there is no easy way to download media\n")
         }
 
     }
